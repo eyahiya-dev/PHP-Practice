af4a51ea1d6d5b15024a9023aa104a22df01e201
Yahya Mahmud <122890225+eyahiya-dev@users.noreply.github.com>
2023-10-21 01:03:27 +0600

    add inheritance file

---
+++ OOP - PHP/simple Assignment - OOP/inheritance.php
@@ -0,0 +1,10 @@
+<?php
+/*
+
+Assignment 2: Inheritance
+
+* Create a base class named "Shape" with properties for color and area.
+* Create two child classes, "Circle" and "Rectangle," that inherit from "Shape" and have methods to calculate their respective areas.
+* Create objects for both child classes and display their properties.
+
+*/
