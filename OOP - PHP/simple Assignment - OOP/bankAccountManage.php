b1a2350c945ac9762fe437d3468aad23abc842c1
Yahya Mahmud <122890225+eyahiya-dev@users.noreply.github.com>
2023-10-23 18:56:07 +0600

    add BankAccountManagement file in PHP OOP

--- OOP - PHP/simple Assignment - OOP/bankAccountManage.php
+++ OOP - PHP/simple Assignment - OOP/bankAccountManage.php
@@ -7,32 +7,48 @@ Create a BankAccount class with properties like account number, account holder n
 
 */
 class BankAccount{
-    public  $accountNumber;
-    public  $accountHolderName;
-    public  $balance;
+    private $accountNumber;
+    private $accountHolderName;
+    private $balance;
 
-    public  function setAccountHolderName($accountHolderName) {
-        $this->accountHolderName = $accountHolderName;
-    }
-    public  function setAccountNumber($accountNumber) {
+    public function __construct($accountNumber, $accountHolderName, $balance){
         $this->accountNumber = $accountNumber;
-    }
-    public  function setBalance($balance) {
+        $this->accountHolderName = $accountHolderName;
         $this->balance = $balance;
     }
+    
+    // Deposite method
+    public function deposit($amount){
+        if($amount > 0){
+            $this->balance += $amount;
+            return "Deposited {$amount}. New Balance : {$this->balance}";
+        }else{
+            return "Invalid Deposited amount. Amount must be greater than ZERO";
+        }
+    }
 
-    public  function getAccountNumber() {
-        return $this->accountNumber;
-    }
-    public  function getAccountHolderName(){
-        return $this->accountHolderName;
-    }
-    public  function getBalance(){
-        return $this->balance;
+    // Withdrawal Method
+    public function withdraw($amount){
+        if($amount > 0){
+            if($this->balance >= $amount){
+                $this->balance -= $amount;
+                return "Withdraw : {$amount}. New amount : {$this->balance}";
+            }else{
+                return "Insufficient Balance. Your Current Balance : {$this->balance}";
+            }
+        }else{
+            return "Invalid withdrawal amount. Amount must be greater than ZERO";
+        }
     }
 
+    // Balance Inqueri Method
+    public function balanceInquery(){
+        return "Account Number : {$this->accountNumber}, Name : {$this->accountHolderName}, Balance : {$this->balance}";
+    }
 }
 
-$bankStatement = new BankAccount();
-$bankStatement->setAccountHolderName("Yahya Mahmud");
-echo $bankStatement->getAccountHolderName();
+$account = new BankAccount('123456', 'Yahya Mahmud', 5000);
+$account->deposit(2000);
+$account->withdraw(500);
+$checkAccount = $account->balanceInquery();
+echo $checkAccount;
