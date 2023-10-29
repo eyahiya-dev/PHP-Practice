10cae79d58a0735203112b2cc0f003180d081423
Yahya Mahmud <122890225+eyahiya-dev@users.noreply.github.com>
2023-10-29 23:23:07 +0600

    add OPP file

--- Sesseion - PHP/sessionBasic.php
+++ Sesseion - PHP/sessionBasic.php
@@ -2,3 +2,5 @@
 //  if you want to code with session, you should start session firts with session_start() function;
 session_start();
 // role management 
+// request response model
+// json is a data format.
