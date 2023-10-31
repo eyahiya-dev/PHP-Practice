4848fd70080138fc961f96d7f22b264d45716898
Yahya Mahmud <122890225+eyahiya-dev@users.noreply.github.com>
2023-10-31 22:40:19 +0600

    add OPP file

--- JSON - PHP/serverMethod.php
+++ JSON - PHP/serverMethod.php
@@ -1,7 +1,10 @@
 <?php
 // post method
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    echo "This is POST Method";
+    session_start();
+    $_SESSION['username'] = "Yahya Mahmud";
+    echo "Set User Name with session";
+
 }
 // json encode and json decode should practice
 
