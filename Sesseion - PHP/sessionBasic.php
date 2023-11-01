3e28a4f0b061164b70cbaeaa3533032bb3754eeb
Yahya Mahmud <122890225+eyahiya-dev@users.noreply.github.com>
2023-11-01 21:51:34 +0600

    add session file

--- Sesseion - PHP/sessionBasic.php
+++ Sesseion - PHP/sessionBasic.php
@@ -1,7 +1,14 @@
 <?php
 //  if you want to code with session, you should start session firts with session_start() function;
 session_start();
-// role management 
-// request response model
-// json is a data format.
-// 
+$inputName = "yahya";
+$inputemail = "yahya@gmail.com";
+$inputrole = "admin";
+
+$name = $_SESSION["userName"] = $inputName;
+$email = $_SESSION["email"] = $inputemail;
+$role = $_SESSION["role"] = $inputrole;
+
+echo $name;
+echo $email;
+echo $role;
