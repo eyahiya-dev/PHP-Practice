44fc1d6be8d2af250964e34861101180e07b9718
Yahya Mahmud <122890225+eyahiya-dev@users.noreply.github.com>
2023-11-01 20:41:17 +0600

    add OPP file

--- JSON - PHP/serverMethod.php
+++ JSON - PHP/serverMethod.php
@@ -1,14 +1,35 @@
 <?php
-// post method
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    session_start();
-    $_SESSION['username'] = "Yahya Mahmud";
-    echo "Set User Name with session";
+/*
 
-}
-// json encode and json decode should practice
+What is JSON?
+JSON stands for JavaScript Object Notation
+JSON is a lightweight data interchange format
+JSON is language independent *
+JSON is "self-describing" and easy to understand
+* The JSON syntax is derived from JavaScript object notation syntax, but the JSON format is text only. Code for reading and generating JSON data can be written in any programming language.
 
-// get method
-if($_SERVER['REQUEST_METHOD'] == "GET"){
-    echo "This is GET method";
+#refrence - W3 School.
+
+
+
+Example of JSON.
+
+{
+    "employees":[
+      {"firstName":"John", "lastName":"Doe"},
+      {"firstName":"Anna", "lastName":"Smith"},
+      {"firstName":"Peter", "lastName":"Jones"}
+    ]
 }
+
+*/
+
+// JSON in PHP, Json containd object , this object is called associative array in PHP
+
+$associtiveArray = [
+    ["fName" => "Yahya","lName"=> "mahmud","email"=> "yahya@gmail.com"],
+    ["fName" => "Mahmuda","lName"=> "akter","email"=> "mahmuda@gmail.com"],
+    ["fName" => "Nazma","lName"=> "begum","email"=> "nazma@gmail.com"],
+];
+
+// this is a associative array
